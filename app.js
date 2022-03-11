e0872eb77850635dfedff58fd6f743eeeb29f9dd
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-03-11 22:55:26 +0300

    added js

---
+++ app.js
@@ -0,0 +1,8 @@
+let go = document.getElementById('go');
+
+go.addEventListener('click', () =>{
+let input = document.getElementById('input').value;
+console.log(input);
+})
+
+let kP = document.getElementById(k-p);
