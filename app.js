80dfa8a1623a9120565ffc4703d29d97350654b3
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-03-12 11:42:39 +0300

    forgot to change the old id name from js

--- app.js
+++ app.js
@@ -2,23 +2,24 @@
 
 /* kilograms to pound 
 Eq = Equation */
+
 let btn = document.getElementById('go');
 btn.addEventListener('click', () =>{
 let input = document.getElementById('input').value;
-let kiloToPoun = document.getElementById('k-p');
+let kiloToPoun = document.getElementById('kiloToPoun');
 let kiloToPounEq = input * 2.205;
 kiloToPoun.textContent += `${input} kilos = ${kiloToPounEq.toFixed(3)} pounds`;
 console.log(kiloToPoun);
 })
 
-// // ENTER KEY EVENTLISTENER
-// let classInput = document.querySelector('.inputt');
-// classInput.addEventListener('keydown', (e) =>{
-// if( e.key === 'Enter'){
-// let input = document.getElementById('input').value;
-// let kiloToPoun = document.getElementById('k-p');
-// let kiloToPounEq = input * 2.205;
-// kiloToPoun.textContent += `${input} kilos = ${kiloToPounEq.toFixed(3)} pounds`;
-// console.log(kiloToPoun);
-// }
-// })
+// ENTER KEY EVENTLISTENER
+let classInput = document.querySelector('.inputt');
+classInput.addEventListener('keydown', (e) =>{
+if( e.key === 'Enter'){
+let input = document.getElementById('input').value;
+let kiloToPoun = document.getElementById('kiloToPoun');
+let kiloToPounEq = input * 2.205;
+kiloToPoun.textContent += `${input} kilos = ${kiloToPounEq.toFixed(3)} pounds`;
+console.log(kiloToPoun);
+}
+})
