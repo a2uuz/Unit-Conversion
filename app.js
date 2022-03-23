4e109ecbde1c382f83fa052b6cf36635aa383424
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-03-23 15:55:53 +0300

    fixed generating output  issues

--- app.js
+++ app.js
@@ -9,31 +9,31 @@ let input = document.getElementById('input').value;
 // LENGHT
 let meterToFeet = document.getElementById('meterToFeet');
 let meterToFeetEq = input * 3.281;
-meterToFeet.textContent += `${input} meters = ${meterToFeetEq.toFixed(3)} feet`;
+meterToFeet.textContent = `${input} meters = ${meterToFeetEq.toFixed(3)} feet`;
 // LENGHT
 let feetToMeter = document.getElementById('feetToMeter');
 let feetToMeterEq = input / 3.281;
-feetToMeter.textContent += `${input} feet = ${feetToMeterEq.toFixed(3)} meters`;
+feetToMeter.textContent = `${input} feet = ${feetToMeterEq.toFixed(3)} meters`;
 
 
 // VOLUME
 let literToGallo = document.getElementById('literToGallo');
 let literToGalloEq = input / 3.785;
-literToGallo.textContent += `${input} liters = ${literToGalloEq.toFixed(3)} gallons`;
+literToGallo.textContent = `${input} liters = ${literToGalloEq.toFixed(3)} gallons`;
 // VOLUME
 let galloToLiter = document.getElementById('galloToLiter');
 let galloToLiterEq = input * 3.785;
-galloToLiter.textContent += `${input} gallons = ${galloToLiterEq.toFixed(3)} liters`;
+galloToLiter.textContent = `${input} gallons = ${galloToLiterEq.toFixed(3)} liters`;
 
 
 // MASS
 let kiloToPoun = document.getElementById('kiloToPoun');
 let kiloToPounEq = input * 2.205;
-kiloToPoun.textContent += `${input} kilos = ${kiloToPounEq.toFixed(3)} pounds`;
+kiloToPoun.textContent = `${input} kilos = ${kiloToPounEq.toFixed(3)} pounds`;
 // MASS
 let pounToKilo = document.getElementById('pounToKilo');
 let pounToKiloEq = input / 2.205;
-pounToKilo.textContent += `${input} pounds = ${pounToKiloEq.toFixed(3)} kilos`;
+pounToKilo.textContent = `${input} pounds = ${pounToKiloEq.toFixed(3)} kilos`;
 
 })
 
@@ -46,31 +46,31 @@ let input = document.getElementById('input').value;
 // LENGHT
 let meterToFeet = document.getElementById('meterToFeet');
 let meterToFeetEq = input * 3.281;
-meterToFeet.textContent += `${input} meters = ${meterToFeetEq.toFixed(3)} feet`;
+meterToFeet.textContent = `${input} meters = ${meterToFeetEq.toFixed(3)} feet`;
 // LENGHT
 let feetToMeter = document.getElementById('feetToMeter');
 let feetToMeterEq = input / 3.281;
-feetToMeter.textContent += `${input} feet = ${feetToMeterEq.toFixed(3)} meters`;
+feetToMeter.textContent = `${input} feet = ${feetToMeterEq.toFixed(3)} meters`;
 
 
 // VOLUME
 let literToGallo = document.getElementById('literToGallo');
 let literToGalloEq = input / 3.785;
-literToGallo.textContent += `${input} liters = ${literToGalloEq.toFixed(3)} gallons`;
+literToGallo.textContent = `${input} liters = ${literToGalloEq.toFixed(3)} gallons`;
 // VOLUME
 let galloToLiter = document.getElementById('galloToLiter');
 let galloToLiterEq = input * 3.785;
-galloToLiter.textContent += `${input} gallons = ${galloToLiterEq.toFixed(3)} liters`;
+galloToLiter.textContent = `${input} gallons = ${galloToLiterEq.toFixed(3)} liters`;
 
 
 // MASS
 let kiloToPoun = document.getElementById('kiloToPoun');
 let kiloToPounEq = input * 2.205;
-kiloToPoun.textContent += `${input} kilos = ${kiloToPounEq.toFixed(3)} pounds`;
+kiloToPoun.textContent = `${input} kilos = ${kiloToPounEq.toFixed(3)} pounds`;
 // MASS
 let pounToKilo = document.getElementById('pounToKilo');
 let pounToKiloEq = input / 2.205;
-pounToKilo.textContent += `${input} pounds = ${pounToKiloEq.toFixed(3)} kilos`;
+pounToKilo.textContent = `${input} pounds = ${pounToKiloEq.toFixed(3)} kilos`;
 
 }
 })
